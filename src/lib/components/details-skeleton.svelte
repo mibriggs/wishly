8898339ab0bcd7f9486559f2d5e5a4e1793008e6
mibriggs <33757644+mibriggs@users.noreply.github.com>
2025-08-28 17:26:19 -0500

    changed skeleton color

--- src/lib/components/details-skeleton.svelte
+++ src/lib/components/details-skeleton.svelte
@@ -5,19 +5,19 @@
 
 <div role="status" class="animate-pulse px-4 pt-8" out:fade>
 	<div class="mb-4 flex items-center gap-4">
-		<span class="h-6 w-2/12 rounded-md bg-slate-400 opacity-70"></span>
+		<span class="h-6 w-2/12 rounded-md bg-zinc-400 opacity-70"></span>
 		<ImageIcon class="h-6 w-6" />
 	</div>
 
-	<div class="mb-1 h-6 w-1/5 rounded-md bg-slate-400 opacity-70"></div>
+	<div class="mb-1 h-6 w-1/5 rounded-md bg-zinc-400 opacity-70"></div>
 
 	<div class="mb-4 flex w-full flex-col gap-3">
-		<span class="h-10 w-full rounded-md border-2 bg-slate-400 p-2 opacity-70 md:w-3/4 lg:w-1/2"
+		<span class="h-10 w-full rounded-md border-2 bg-zinc-400 p-2 opacity-70 md:w-3/4 lg:w-1/2"
 		></span>
 		<div class="flex items-center gap-4">
-			<span class="h-10 w-[96px] rounded-md border-2 bg-slate-400 p-2 opacity-70"></span>
-			<span class="h-10 w-1/3 rounded-md border-2 bg-slate-400 p-2 opacity-70"></span>
-			<span class="h-10 w-[96px] rounded-md border-2 bg-slate-400 p-2 opacity-70"></span>
+			<span class="h-10 w-[96px] rounded-md border-2 bg-zinc-400 p-2 opacity-70"></span>
+			<span class="h-10 w-1/3 rounded-md border-2 bg-zinc-400 p-2 opacity-70"></span>
+			<span class="h-10 w-[96px] rounded-md border-2 bg-zinc-400 p-2 opacity-70"></span>
 		</div>
 	</div>
 
@@ -28,7 +28,7 @@
 			</li>
 		{/each}
 		<span
-			class="mt-4 flex h-6 w-1/4 items-center justify-center gap-1 rounded-md bg-slate-400 px-6 py-1 md:w-1/5 lg:w-1/12"
+			class="mt-4 flex h-6 w-1/4 items-center justify-center gap-1 rounded-md bg-zinc-400 px-6 py-1 md:w-1/5 lg:w-1/12"
 		>
 		</span>
 	</ul>
@@ -37,7 +37,7 @@
 {#snippet skeletonItemComponent()}
 	<!-- everywhere else -->
 	<div
-		class="hidden h-24 w-11/12 items-center justify-start gap-4 rounded-md bg-slate-400 px-8 py-3 opacity-70 sm:flex md:w-3/4 xl:w-1/3"
+		class="hidden h-24 w-11/12 items-center justify-start gap-4 rounded-md bg-zinc-400 px-8 py-3 opacity-70 sm:flex md:w-3/4 xl:w-1/3"
 	>
 		<ImageIcon class="h-6 w-6" />
 		<span class="ml-auto flex items-center gap-4">
@@ -48,9 +48,8 @@
 
 	<!-- phone screens -->
 	<div
-		class="flex h-20 w-full items-start justify-between gap-4 rounded-md border bg-slate-400 p-3 opacity-70 sm:hidden"
+		class="flex h-20 w-full items-start justify-between gap-4 rounded-md border bg-zinc-400 p-3 opacity-70 sm:hidden"
 	>
-		<!-- <p class="rounded-md bg-neutral-300 px-4 py-3">📦</p> -->
 		<ImageIcon class="h-5 w-5" />
 
 		<div class="flex gap-2 self-end">
